8d1b0ac104761011c740ef34875f3f496abe91eb
samuel1234ng <101535744+samuel1234ng@users.noreply.github.com>
2022-10-26 21:12:03 +0800

    Update Cinema.java

--- SC2002 OODP Assignment/src/Cinema.java
+++ SC2002 OODP Assignment/src/Cinema.java
@@ -1,21 +1,58 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cinema {
-	Movielisting []movie=new Movielisting[8];
+	String []movie=new String[8];
 	int numEmptySeat;
 	
-	Cinema(String cinemaname)
+	Cinema()
 	{
-		String name = cinemaname;
 		for(int j=0;j<8;j++)
 		{
-			movie[j]= new Movielisting();
+			movie[j]= "";
 		}
 	}
 	
-	void Createcinemashowtime(int time, Movielisting movielisting)
+	void storesettings()
+	{
+		ArrayList<String> data = new ArrayList<String>();
+		
+		for(int j=0;j<8;j++)
+		{
+			data.add(movie[j]);
+			data.add("\n");
+		}
+		
+		try {
+			DbIO.writeFile("CinemaSettings", data);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	void loadsettings()
+	{
+		List<String> data = null;
+		try {
+			data=DbIO.readFile("CinemaSettings");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(int j=0;j<8;j++)
+		{
+			movie[j]= data.get(j);
+		}
+	}
+	
+	void Createcinemashowtime(int time, String movielisting)
 	{
 		int timeslot=(time-800)/200;
-		if (movie[timeslot].name=="")
+		if (movie[timeslot]=="")
 		{
 			movie[timeslot]=movielisting;
 		}
@@ -24,10 +61,10 @@ public class Cinema {
 			System.out.print("There is already a movie schedule at this time \n");
 		}
 	}
-	void Updatecinemashowtime(int time, Movielisting movielisting)
+	void Updatecinemashowtime(int time, String movielisting)
 	{
 		int timeslot=(time-800)/200;
-		if (movie[timeslot].name=="")
+		if (movie[timeslot]=="")
 		{
 			movie[timeslot]=movielisting;
 		}
@@ -39,9 +76,9 @@ public class Cinema {
 	void Removecinemashowtime(int time)
 	{
 		int timeslot=(time-800)/200;
-		movie[timeslot].Removemovielisting();
-		movie[timeslot]=new Movielisting();
+		movie[timeslot]="";
 		
 	}
 
 }
+
