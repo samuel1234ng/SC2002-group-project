6025e2654c2d6c4914654a7912702446cdeb2395
YQ0195 <103987256+YQ0195@users.noreply.github.com>
2022-11-02 15:24:14 +0800

    Update Timeslots.java

--- SC2002 OODP Assignment/src/Timeslots.java
+++ SC2002 OODP Assignment/src/Timeslots.java
@@ -1,10 +1,14 @@
-
-
 public class timeSlots {
 	int timing;
 	Seat[][] seating;
 	
+	Seat[][] getSeats(){
+		return this.seating;
+		
+	}
+public setSeats(Seat[][] seatings) {
+	this.seating= seatings;
 	
-	
+}
 
 }
