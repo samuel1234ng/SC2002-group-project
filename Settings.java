d07e8143967d72d1e6b8239ec256cd488a93dfbd
Dumbledore66 <97513829+Dumbledore66@users.noreply.github.com>
2022-11-10 16:21:52 +0800

    Update Settings.java

--- Settings.java
+++ Settings.java
@@ -5,155 +5,134 @@ import java.util.List;
 
 public class Settings {
 
-	int baseticketprice;
-	int []typemod=new int[3];
-	int []classmod=new int[3];
-	int []agemod=new int[3];
-	int []daymod=new int[3];
-	
-	int []holidays=new int[365];
+	int baseTicketPrice;
+	int [] typeMod = new int[3];
+	int [] seatMod = new int[2];
+	int [] ageMod = new int[3];
+	int [] dayMod = new int[3];
+	int [] holidays = new int[365];
+
 	
 	Settings()
 	{
 		for(int j=0;j<365;j++)
 		{
-			holidays[j]= 0;
+			holidays[j] = 0;
 		}
-		holidays[299]=1;
+		holidays[299] =  1;
 
-		baseticketprice=10;
-		
-		typemod[0]=0;//imax2d
-		typemod[1]=2;//imax3d
-		typemod[2]=3;//Blockbuster
-		
-		classmod[0]=0;//normal
-		classmod[1]=2;//Elite 
-		//classmod[2]=-2;//Budget
+		baseTicketPrice = 10;
 
-		agemod[0]=0;//adult
-		agemod[1]=-2;//child
-		agemod[2]=-1;//senior citizen
+		typeMod[0] = 0; // imax2d
+		typeMod[1] = 2; // imax3d
+		typeMod[2] = 3; // Blockbuster
 
-		daymod[0]=0;//weekday
-		daymod[1]=2;//weekend
-		daymod[2]=5;//holiday
+		seatMod[0] = 0; // normal
+		seatMod[1] = 2; // Elite
+
+		ageMod[0] = 0; //adult
+		ageMod[1] = -2; //child
+		ageMod[2] = -1; //senior citizen
+
+		dayMod[0] = 0; //weekday
+		dayMod[1] = 2; //weekend
+		dayMod[2] = 5; //holiday
 	}
-	public void storesettings()
+	public void storeSettings()
 	{
 		ArrayList<String> data = new ArrayList<String>();
-		data.add(Integer.toString(baseticketprice));
+		data.add(Integer.toString(baseTicketPrice));
 		data.add("\n");
-		data.add(Integer.toString(typemod[0]));
+		data.add(Integer.toString(typeMod[0]));
 		data.add("\n");
-		data.add(Integer.toString(typemod[1]));
+		data.add(Integer.toString(typeMod[1]));
 		data.add("\n");
-		data.add(Integer.toString(typemod[2]));
+		data.add(Integer.toString(typeMod[2]));
 		data.add("\n");
-		data.add(Integer.toString(classmod[0]));
+		data.add(Integer.toString(seatMod[0]));
 		data.add("\n");
-		data.add(Integer.toString(classmod[1]));
+		data.add(Integer.toString(seatMod[1]));
 		data.add("\n");
-		//data.add(Integer.toString(classmod[2]));
-		//data.add("\n");
-		data.add(Integer.toString(agemod[0]));
+		data.add(Integer.toString(ageMod[0]));
 		data.add("\n");
-		data.add(Integer.toString(agemod[1]));
+		data.add(Integer.toString(ageMod[1]));
 		data.add("\n");
-		data.add(Integer.toString(agemod[2]));
+		data.add(Integer.toString(ageMod[2]));
 		data.add("\n");
-		data.add(Integer.toString(daymod[0]));
+		data.add(Integer.toString(dayMod[0]));
 		data.add("\n");
-		data.add(Integer.toString(daymod[1]));
+		data.add(Integer.toString(dayMod[1]));
 		data.add("\n");
-		data.add(Integer.toString(daymod[2]));
-		
-		
+		data.add(Integer.toString(dayMod[2]));
+
 		try {
 			SettingsDB.writeFile("data/settings.txt", data);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		ArrayList<String> data2 = new ArrayList<String>();
+
 		for(int j=0;j<365;j++)
 		{
-			data2.add(Integer.toString(holidays[j]));
+			data2.add(String.valueOf(holidays[j]));
 			data2.add("\n");
 		}
-		
-		
+
 		try {
 			SettingsDB.writeFile("data/holidays.txt", data2);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
-	void loadsettings()
+	public void loadSettings()
 	{
 		List<String> data = null;
 		try {
 			data=SettingsDB.readFile("data/settings.txt");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		baseticketprice=Integer.parseInt(data.get(0));
-		typemod[0]=Integer.parseInt(data.get(1));
-		typemod[1]=Integer.parseInt(data.get(2));
-		typemod[2]=Integer.parseInt(data.get(3));
-		classmod[0]=Integer.parseInt(data.get(4));
-		classmod[1]=Integer.parseInt(data.get(5));
-		classmod[2]=Integer.parseInt(data.get(6));
-		agemod[0]=Integer.parseInt(data.get(7));
-		agemod[1]=Integer.parseInt(data.get(8));
-		agemod[2]=Integer.parseInt(data.get(9));
-		daymod[0]=Integer.parseInt(data.get(10));
-		daymod[1]=Integer.parseInt(data.get(11));
-		daymod[2]=Integer.parseInt(data.get(12));
+		baseTicketPrice = Integer.parseInt(data.get(0));
+		typeMod[0] = Integer.parseInt(data.get(1));
+		typeMod[1] = Integer.parseInt(data.get(2));
+		typeMod[2] = Integer.parseInt(data.get(3));
+		seatMod[0] = Integer.parseInt(data.get(4));
+		seatMod[1] = Integer.parseInt(data.get(5));
+		ageMod[0] = Integer.parseInt(data.get(6));
+		ageMod[1] = Integer.parseInt(data.get(7));
+		ageMod[2] = Integer.parseInt(data.get(8));
+		dayMod[0] = Integer.parseInt(data.get(9));
+		dayMod[1] = Integer.parseInt(data.get(10));
+		dayMod[2] = Integer.parseInt(data.get(11));
 		
 		List<String> data2 = null;
 		try {
 			data2=SettingsDB.readFile("data/holidays.txt");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		for(int j=0;j<365;j++)
 		{
-			holidays[j]=Integer.parseInt(data2.get(j));
+			holidays[j] = Integer.parseInt(data2.get(j));
 		}
 	}
-	void printsettings()
+	public void printSettings()
 	{
-		System.out.print("Imax 2D movie price modifier: ");
-		System.out.print(Integer.toString(typemod[0]));
-		System.out.print("\nImax 3D movie price modifier: ");
-		System.out.print(Integer.toString(typemod[1]));
-		System.out.print("\nBlockbuster movie price modifier: ");
-		System.out.print(Integer.toString(typemod[2]));
-		System.out.print("\nNormal seat price modifier: ");
-		System.out.print(Integer.toString(classmod[0]));
-		System.out.print("\nElite seat price modifier: ");
-		System.out.print(Integer.toString(classmod[1]));
-		//System.out.print("\nBudget class price modifier: ");
-		//System.out.print(Integer.toString(classmod[2]));
-		System.out.print("\nadult price modifier: ");
-		System.out.print(Integer.toString(agemod[0]));
-		System.out.print("\nchild price modifier: ");
-		System.out.print(Integer.toString(agemod[1]));
-		System.out.print("\nsenior citizen price modifier: ");
-		System.out.print(Integer.toString(agemod[2]));
-		System.out.print("\nweekday price modifier: ");
-		System.out.print(Integer.toString(daymod[0]));
-		System.out.print("\nweekend price modifier: ");
-		System.out.print(Integer.toString(daymod[1]));
-		System.out.print("\nholiday class price modifier: ");
-		System.out.print(Integer.toString(daymod[2]));
-		System.out.print("\nholidays:");
+		System.out.println("Base Ticket Price: $" + baseTicketPrice);
+		System.out.println("IMAX 2D movie price modifier: " + typeMod[0]);
+		System.out.println("IMAX 3D movie price modifier: " + typeMod[1]);
+		System.out.println("Blockbuster movie price modifier: " + typeMod[2]);
+		System.out.println("Normal seat price modifier: " + seatMod[0]);
+		System.out.println("Elite seat price modifier: " + seatMod[1]);
+		System.out.println("Adult price modifier: " + ageMod[0]);
+		System.out.println("Child price modifier: " + ageMod[1]);
+		System.out.println("Senior Citizen price modifier: " + ageMod[2]);
+		System.out.println("Weekday price modifier: " + dayMod[0]);
+		System.out.println("Weekend price modifier: " + dayMod[1]);
+		System.out.println("Holiday price modifier: " + dayMod[2]);
+		System.out.print("Holidays: ");
 		for(int j=0;j<365;j++)
 		{
 			if (holidays[j]==1)
@@ -162,42 +141,45 @@ public class Settings {
 				System.out.print(" ");
 			}
 		}
+		System.out.println();
+		System.out.println();
 	}
-	void addholiday(int day)
+	public void addHoliday(int day)
 	{
-		holidays[day]=1;
+		holidays[day] = 1;
 	}
-	void removeholiday(int day)
+	public void removeHoliday(int day)
 	{
-		holidays[day]=0;
+		holidays[day] = 0;
 	}
-	void changebaseprice(int newprice)
+	public void changeBasePrice(int newPrice)
 	{
-		baseticketprice=newprice;
+		baseTicketPrice = newPrice;
 	}
 	
-	void changeticketprice(int setting,int option,int value)
+	public void changeTicketPrice(int setting,int option,int value)
 	{
 		if (setting==1)
 		{
-			typemod[option-1]=value;
+			typeMod[option-1]=value;
 		}
 		if (setting==2)
 		{
-			classmod[option-1]=value;
+			seatMod[option-1]=value;
 		}
 		if (setting==3)
 		{
-			agemod[option-1]=value;
+			ageMod[option-1]=value;
 		}
 		if (setting==4)
 		{
-			daymod[option-1]=value;
+			dayMod[option-1]=value;
 		}
 	}
-	int calculateticketprice(int cinematype,int cinemaclass, int age, int day)
+
+	public int calculateTicketPrice(int cinemaType,int cinemaClass, int age, int day)
 	{
-		return baseticketprice+typemod[cinematype]+classmod[cinemaclass]+agemod[age]+daymod[day];
+		return baseTicketPrice + typeMod[cinemaType]+ seatMod[cinemaClass] + ageMod[age] + dayMod[day];
 	}
 	
 
