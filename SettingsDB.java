2e02ca01b0b8c63cfe2a7f4d1155633af617c86f
samuel1234ng <101535744+samuel1234ng@users.noreply.github.com>
2022-11-11 23:23:55 +0800

    Update SettingsDB.java

--- SettingsDB.java
+++ SettingsDB.java
@@ -3,14 +3,25 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-
+/**
+ * Reads and writes from Settings file
+ * @author Samuel Ng
+ * @version 1.0
+ * @since 2022-11-05
+ *
+ */
 public class SettingsDB {
 
     /**
      * The character that separates entries in the text file.
      */
     public static final String SEPARATOR = "|";
-
+    /**
+     * Writes setting to file
+     * @param fileName settings file location
+     * @param data data to store
+     * @throws IOException
+     */
     public static void writeFile(String fileName, ArrayList<String> data) throws IOException {
         File file = new File(fileName);
         try (FileWriter out = new FileWriter(file, false)) {
@@ -20,7 +31,12 @@ public class SettingsDB {
         }
     }
 
-
+    /**
+     * Reads settings from file
+     * @param fileName settings file location
+     * @return data from file
+     * @throws FileNotFoundException
+     */
     public static ArrayList<String> readFile(String fileName) throws FileNotFoundException {
         File f = new File(fileName);
         ArrayList<String> data = new ArrayList<>();
