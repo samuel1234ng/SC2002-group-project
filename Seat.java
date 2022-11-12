a3a2e433c8027848fcca34d854ae28c0375f84a4
samuel1234ng <101535744+samuel1234ng@users.noreply.github.com>
2022-11-12 12:46:01 +0800

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -1,3 +1,10 @@
+/**
+ * Class that stores the booking status of seats
+ * @author Samuel Ng
+ * @version 1.0
+ * @since 2022-11-05
+ *
+ */
 public class Seat {
     //class attributes
     private final String seatId;
@@ -12,23 +19,45 @@ public class Seat {
 
 
     //methods
+   /**
+    *  Returns seat ID of this seat
+    * @return ID of seat
+    */
     public String getSeatId(){
         return this.seatId;
     }
+    /**
+     * 
+     * @return
+     */
     public String getViewerId(){
         return this.viewerId;
     }
+    /**
+     * Returns seat ID of viewer that booked this seat
+     * @return ID of viewer
+     */
     public boolean isAssigned(){
         return this.assigned;
     }
+    /**
+     * Book the seat
+     * @param viewer_id viewer that booked the seat
+     */
     public void assign(String viewer_id){
         this.viewerId = viewer_id;
         this.assigned = true;
     }
 
+    /**
+     * change the status of the seat to booked
+     */
     public void assign(){
         this.assigned = true;
     }
+    /**
+     * change the status of the seat to not booked, remove viewer ID
+     */
     public void unassign(){
         this.viewerId = "0";
         this.assigned = false;
